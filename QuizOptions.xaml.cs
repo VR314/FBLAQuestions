0ea6a7a614bcc7617a461c3df234cb097a6968b2
VR314 <raghupathivaroon@gmail.com>
2021-05-09 18:27:48 -0400

    [ui] => add error checking for no boxes checked in QuizOptions

--- QuizOptions.xaml.cs
+++ QuizOptions.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
- 
+
 namespace FBLAQuestions {
     /// <summary>
     /// Interaction logic for QuizOptions.xaml
@@ -22,16 +23,20 @@ namespace FBLAQuestions {
             object tfChoice = FindName("TFBox");
             object dropChoice = FindName("DropdownBox");
 
-            if (mcChoice is CheckBox && fillChoice is CheckBox && tfChoice is CheckBox && dropChoice is CheckBox) {
-                mainWindow.Options[0] = (bool)((CheckBox)mcChoice).IsChecked;
-                mainWindow.Options[1] = (bool)((CheckBox)fillChoice).IsChecked;
-                mainWindow.Options[2] = (bool)((CheckBox)tfChoice).IsChecked;
-                mainWindow.Options[3] = (bool)((CheckBox)dropChoice).IsChecked;
+            if (mcChoice is CheckBox mcBox && fillChoice is CheckBox fillBox && tfChoice is CheckBox tfBox && dropChoice is CheckBox dropBox) {
+                mainWindow.Options[0] = (bool)mcBox.IsChecked;
+                mainWindow.Options[1] = (bool)fillBox.IsChecked;
+                mainWindow.Options[2] = (bool)tfBox.IsChecked;
+                mainWindow.Options[3] = (bool)dropBox.IsChecked;
             } else {
                 throw new System.InvalidOperationException("CheckBoxes not retreived correctly");
             }
 
-            mainWindow.Navigate("Quiz.xaml");
+            if (mainWindow.Options.Contains(true)) {
+                mainWindow.Navigate("Quiz.xaml");
+            } else {
+                MessageBox.Show("Choose at least one type of question!");
+            }
         }
     }
 }

418af9cab6a52d761fc1c71220bc080d968b8724
VR314 <raghupathivaroon@gmail.com>
2021-05-09 18:28:12 -0400

    [ui] => add back button to questions viewer

--- Viewer.xaml.cs
+++ Viewer.xaml.cs
@@ -1,15 +1,21 @@
 ﻿using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
- 
+
 namespace FBLAQuestions {
     /// <summary>
     /// Interaction logic for Viewer.xaml
     /// </summary>
     public partial class Viewer : Page {
+        /// <summary>
+        /// The reference to the MainWindow and its variables
+        /// </summary>
+        private MainWindow mainWindow;
         public Viewer() {
             InitializeComponent();
+            mainWindow = (MainWindow)Application.Current.MainWindow;
             var MCs = DatabaseConnection.LoadAllQuestions(new QuestionType[] { QuestionType.MC, QuestionType.Dropdown });
-            BindingList<DisplayQuestion> MCbl = new BindingList<DisplayQuestion>();
+            BindingList<DisplayQuestion> MCbl = new();
             foreach (var MC in MCs) {
                 DisplayQuestion dq = new(MC);
                 MCbl.Add(dq);
@@ -17,12 +23,16 @@ namespace FBLAQuestions {
             gridMC.ItemsSource = MCbl;
 
             var TFs = DatabaseConnection.LoadAllQuestions(new QuestionType[] { QuestionType.TF, QuestionType.FillIn });
-            BindingList<DisplayQuestion> TFbl = new BindingList<DisplayQuestion>();
+            BindingList<DisplayQuestion> TFbl = new();
             foreach (var TF in TFs) {
                 DisplayQuestion dq = new(TF);
                 TFbl.Add(dq);
             }
             gridFill.ItemsSource = TFbl;
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e) {
+            mainWindow.Navigate("Menu.xaml");
         }
     }
 }
